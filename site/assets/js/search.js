4c970653b99afd7feef587dc4e93d0b486d15d27
helsetjam <helen.setoodeh@tibble.nu>
2020-06-11 16:02:37 +0200

    Initialize master-mc-opensearch

---
+++ site/assets/js/search.js
@@ -0,0 +1,72 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+(function () {
+  'use strict'
+
+  if (!window.docsearch) {
+    return
+  }
+
+  var inputElement = document.getElementById('search-input')
+  var siteDocsVersion = inputElement.getAttribute('data-docs-version')
+
+  function getOrigin() {
+    var location = window.location
+    var origin = location.origin
+
+    if (!origin) {
+      var port = location.port ? ':' + location.port : ''
+
+      origin = location.protocol + '//' + location.hostname + port
+    }
+
+    return origin
+  }
+
+  var search = window.docsearch({
+    apiKey: '5990ad008512000bba2cf951ccf0332f',
+    indexName: 'bootstrap',
+    inputSelector: '#search-input',
+    algoliaOptions: {
+      facetFilters: ['version:' + siteDocsVersion]
+    },
+    transformData: function (hits) {
+      return hits.map(function (hit) {
+        var currentUrl = getOrigin()
+        var liveUrl = 'https://getbootstrap.com'
+
+        // When in production, return the result as is,
+        // otherwise remove our url from it.
+        // eslint-disable-next-line no-negated-condition
+        hit.url = currentUrl.indexOf(liveUrl) !== -1 ? // lgtm [js/incomplete-url-substring-sanitization]
+          hit.url :
+          hit.url.replace(liveUrl, '')
+
+        // Prevent jumping to first header
+        if (hit.anchor === 'content') {
+          hit.url = hit.url.replace(/#content$/, '')
+          hit.anchor = null
+        }
+
+        return hit
+      })
+    },
+    // Set debug to `true` if you want to inspect the dropdown
+    debug: false
+  })
+
+  function hashSearch() {
+    var SEARCH_WORD = '#search='
+
+    if (window.location.hash && window.location.hash.indexOf(SEARCH_WORD) === 0) {
+      search.input.autocomplete.setVal(window.location.hash.slice(SEARCH_WORD.length))
+      search.input.autocomplete.open()
+    }
+  }
+
+  hashSearch()
+  // For the nerds: search by changing the url hash
+  window.addEventListener('hashchange', hashSearch, false)
+})()
